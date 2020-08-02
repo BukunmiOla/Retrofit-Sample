324302a73cd72916f79aa5003c84a59fc211e731
BukunmiOla <ebukunmiola@gmail.com>
2020-08-02 23:43:35 +0100

    JsonPlaceholder Posts model added

---
+++ app/src/main/java/com/example/retrofit/DummyApiActivity.java
@@ -0,0 +1,14 @@
+package com.example.retrofit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DummyApiActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dummy_api);
+    }
+}
