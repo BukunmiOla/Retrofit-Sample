42cc405a4e56ce03c325d788ede752a4ba6277b5
dekola <akano.adekola@gmail.com>
2020-08-07 15:35:45 +0100

    add http loggin

--- app/src/main/java/com/example/retrofit/network/RetrofitClientInstance.java
+++ app/src/main/java/com/example/retrofit/network/RetrofitClientInstance.java
@@ -1,5 +1,9 @@
 package com.example.retrofit.network;
 
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -15,9 +19,21 @@ public class RetrofitClientInstance {
 
     public static Retrofit getTypicodeClientInstance() {
         if (retrofitTY == null) {
+            HttpLoggingInterceptor logging = new HttpLoggingInterceptor();
+            logging.level(HttpLoggingInterceptor.Level.BODY);
+
+            OkHttpClient.Builder httpClient = new OkHttpClient.Builder();
+
+//        httpClient.enableTls12()
+            httpClient.connectTimeout(30, TimeUnit.SECONDS);
+            httpClient.readTimeout(30, TimeUnit.SECONDS);
+            httpClient.writeTimeout(30, TimeUnit.SECONDS);
+            httpClient.addInterceptor(logging);
+
 
             retrofitTY = new retrofit2.Retrofit.Builder()
                     .baseUrl(TYPICODE_COM)
+                    .client(httpClient.build())
                     .addConverterFactory(GsonConverterFactory.create())
                     .build();
         }
